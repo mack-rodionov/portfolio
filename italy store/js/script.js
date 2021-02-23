0e7b51a2e4b3d1e7f401a49377730b961161bdee
mack-rodionov <75488276+mack-rodionov@users.noreply.github.com>
2021-02-23 16:31:40 +0300

    Add files via upload

---
+++ italy store/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('a[href*=index.html]').bind('click', function(e) {
+	e.preventDefault();
+ 
+	var target = $(this).attr("href");
+ 
+	$('html, body').stop().animate({ 
+		scrollTop: $(target).offset().top
+		}, 500, function() {
+			location.hash = target;
+		});
+ 
+		return false;
+	});
+});
